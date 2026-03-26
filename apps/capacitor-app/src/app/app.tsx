d8263316971a75dd0dd593932ec1b7f62ba7f64f
sjnp <supawet.j@gmail.com>
2026-03-27 00:14:28 +0700

    POC input onFocus at the end

--- apps/capacitor-app/src/app/app.tsx
+++ apps/capacitor-app/src/app/app.tsx
@@ -1,7 +1,51 @@
+import { ChangeEvent, FocusEvent, useRef, useState } from 'react';
+
 export function App() {
+  const maxLength = 20;
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [value, setValue] = useState('');
+  const [clickValue, setClickValue] = useState('');
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const currentValue = event.target.value;
+    if (currentValue.length <= maxLength) {
+      setValue(event.target.value);
+      setClickValue('');
+    }
+  };
+
+  const handleOnClick = () => {
+    setClickValue(value);
+  };
+
+  const handleOnFocus = (event: FocusEvent) => {
+    const input = inputRef.current;
+    if (!input) {
+      return;
+    }
+    setTimeout(() => {
+      input.focus();
+      input.setSelectionRange(-1, -1);
+      input.scrollLeft = input.scrollWidth;
+    }, 0);
+  };
+
   return (
-    <div>
-      <h1 className="pt-20">Hello, world!</h1>
+    <div className="pt-20 flex flex-col gap-3 items-center justify-start">
+      <input
+        className="p-2 border-2 border-blue-500"
+        ref={inputRef}
+        type="text"
+        value={value}
+        onChange={handleChange}
+        onFocus={handleOnFocus}
+      />
+      <p>
+        {value.length}/{maxLength}
+      </p>
+      <button className="p-2 bg-red-700 text-white" onClick={handleOnClick}>
+        Submit
+      </button>
+      <p>Clicked: {clickValue}</p>
     </div>
   );
 }
---
+++ libs/ui/README.md
@@ -0,0 +1,7 @@
+# @sjnp/ui
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @sjnp/ui` to execute the unit tests via [Vitest](https://vitest.dev/).
